9258e0ee680781097cbeca8c9d30a06a66e984a5
Sam Lee <cable8mm@gmail.com>
2023-12-14 00:37:30 +1300

    Conform to the content view protocol

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by samgu.lee on 14/12/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
