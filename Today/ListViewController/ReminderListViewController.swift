b4e442c57436bceb0bf82d72aa3ac36ab46cfaaa
Sam Lee <cable8mm@gmail.com>
2023-12-14 01:18:34 +1300

    Connect the target-action pair

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -28,6 +28,15 @@ class ReminderListViewController: UICollectionViewController {
             )
         }
 
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add, target: self, action: #selector(didPressAddButton(_:)))
+        addButton.accessibilityLabel = NSLocalizedString(
+            "Add reminder", comment: "Add button accessibility label")
+        navigationItem.rightBarButtonItem = addButton
+        if #available(iOS 16, *) {
+            navigationItem.style = .navigator
+        }
+        
         updateSnapshot()
 
         collectionView.dataSource = dataSource
