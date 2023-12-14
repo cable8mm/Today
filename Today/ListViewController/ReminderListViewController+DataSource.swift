bc2de430fdf52095591c19e26e4fd0e21e3fa3f1
Sam Lee <cable8mm@gmail.com>
2023-12-14 20:30:46 +1300

    Respond to change notifications

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -92,6 +92,8 @@ extension ReminderListViewController {
             do {
                 try await reminderStore.requestAccess()
                 reminders = try await reminderStore.readAll()
+                NotificationCenter.default.addObserver(
+                                    self, selector: #selector(eventStoreChanged(_:)), name: .EKEventStoreChanged, object: nil)
             }
             catch TodayError.accessDenied, TodayError.accessRestricted {
                 #if DEBUG
@@ -101,6 +103,13 @@ extension ReminderListViewController {
             catch {
                 showError(error)
             }
+            updateSnapshot()
+        }
+    }
+
+    func reminderStoreChanged() {
+        Task {
+            reminders = try await reminderStore.readAll()
             updateSnapshot()
         }
     }
